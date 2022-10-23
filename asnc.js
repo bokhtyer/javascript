b81b48e6700b02b1ba976007298c591a03011d34
Bokhtyer Abid <68472916+bokhtyer@users.noreply.github.com>
2022-10-23 14:29:50 +0600

    Asynchronous

--- asnc.js
+++ asnc.js
@@ -1,7 +1,17 @@
 const processOrder = (customer) => {
+
     console.log("Processing Order From Customer 1");
-    var currentTime = new Date().getTime();
-    while(currentTime + 3000 >= new Date().getTime());
+
+    // Synchronous
+
+    // var currentTime = new Date().getTime();
+    // while(currentTime + 3000 >= new Date().getTime());
+
+    // Asynchronous
+    setTimeout(()=>{
+        console.log("Cooking Complete");
+    }, 3000);
+
     console.log("Order Process For Customer 1");
 }
 console.log("Take Order From Customer 1");
