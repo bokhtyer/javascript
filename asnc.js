5d62fbd47c12450e67d6d4a8f34e2ca08dbd419d
Bokhtyer Abid <68472916+bokhtyer@users.noreply.github.com>
2022-10-23 12:45:59 +0600

    dev

--- asnc.js
+++ asnc.js
@@ -1,20 +1,9 @@
-// const processOrder = (customer) => {
-//     console.log("Processing Order From Customer 1");
-//     var currentTime = new Date().getTime();
-//     while(currentTime + 3000 >= new Date().getTime());
-//     console.log("Order Process For Customer 1");
-// }
-// console.log("Take Order From Customer 1");
-// processOrder();
-// console.log("Complete Order From Customer 1")
-
-
 const processOrder = (customer) => {
-    console.log("Processing Order Customer 1");
+    console.log("Processing Order From Customer 1");
     var currentTime = new Date().getTime();
-    while(currentTime + 5000 >= new Date().getTime());
-    console.log("Process Your Order");
+    while(currentTime + 3000 >= new Date().getTime());
+    console.log("Order Process For Customer 1");
 }
-console.log("Taka Order From Customer 1")
+console.log("Take Order From Customer 1");
 processOrder();
-console.log("Complete Oder From Customer 1")
+console.log("Complete Order From Customer 1")
